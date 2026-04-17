88244ea8647c1b6ca4dc5f16393b05b7b060a6fa
MajdKhalifa <majdkhalifa321@gmail.com>
2026-04-18 02:19:55 +0300

    fixed saved issues

--- lib/core/widgets/main_navigation.dart
+++ lib/core/widgets/main_navigation.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_project/core/widgets/app_bottom_nav.dart';
 import 'package:flutter_project/features/explore/explore_screen.dart';
 import 'package:flutter_project/features/home/ui/home_screen.dart';
 import 'package:flutter_project/features/my_profile/ui/my_profile.dart';
 import 'package:flutter_project/features/saved/ui/saved_screen.dart';
-import 'package:flutter_project/features/home/cubit/home_cubit.dart';
 import 'package:flutter_project/core/routing/routes.dart';
 import 'package:flutter_project/core/constants/app_color.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_project/features/home/cubit/home_cubit.dart';
+import 'package:flutter_project/features/saved/logic/cubit/saved_articals_cubit.dart';
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -21,38 +22,42 @@ class _MainNavigationState extends State<MainNavigation> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => HomeCubit(),
-      child: Scaffold(
-        body: IndexedStack(
-          index: currentIndex,
-          children: const [
-            HomeScreen(),
-            ExploreScreen(),
-            SavedScreen(),
-            MyProfile(),
-          ],
-        ),
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: const [
+          HomeScreen(),
+          ExploreScreen(),
+          SavedScreen(),
+          MyProfile(),
+        ],
+      ),
 
-        floatingActionButton: FloatingActionButton(
-          heroTag: "main_fab",
-          backgroundColor: AppColor.accent,
-          child: const Icon(Icons.add, color: Colors.white),
-          onPressed: () {
-            Navigator.pushNamed(context, Routes.publishStep1Screen);
-          },
-        ),
+      // ✔️ زر البوست يظهر فقط في الهوم
+      floatingActionButton: currentIndex == 0
+          ? FloatingActionButton(
+              heroTag: "main_fab",
+              backgroundColor: AppColor.accent,
+              child: const Icon(Icons.add, color: Colors.white),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.publishStep1Screen);
+              },
+            )
+          : null,
 
-        bottomNavigationBar: AppBottomNav(
-          currentIndex: currentIndex,
-          onTap: (index) {
-            setState(() => currentIndex = index);
+      bottomNavigationBar: AppBottomNav(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() => currentIndex = index);
 
-            if (index == 0) {
-              context.read<HomeCubit>().loadInitialData();
-            }
-          },
-        ),
+          if (index == 0) {
+            context.read<HomeCubit>().loadInitialData();
+          }
+
+          if (index == 2) {
+            context.read<SavedArticalsCubit>().getSavedArticals();
+          }
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_project/core/routing/app_route.dart';
 import 'package:flutter_project/my_app.dart';
 import 'package:flutter_project/features/home/cubit/home_cubit.dart';
+import 'package:flutter_project/features/saved/logic/cubit/saved_articals_cubit.dart';
+import 'package:flutter_project/features/saved/repo/saved_articals_repo.dart';
+import 'package:flutter_project/core/services/api/api_services.dart';
 
 void main() {
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider<HomeCubit>(create: (_) => HomeCubit()),
-        // إذا عندك Cubits ثانية ضيفها هون
+        BlocProvider<SavedArticalsCubit>(
+          create: (_) =>
+              SavedArticalsCubit(SavedArticalsRepo(api: ApiServices())),
+        ),
       ],
       child: MyApp(appRoute: AppRoute()),
     ),
